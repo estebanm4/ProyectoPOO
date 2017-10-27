43e62708f961f543bdfa795ba841c5095de1cb4f
lenovo <lenovo@10.203.161.244>
2017-10-27 12:03:36 -0500

    v. 0.1.2

--- src/proyectopoo/ProyectoPOO.java
+++ src/proyectopoo/ProyectoPOO.java
@@ -1,11 +1,30 @@
 package proyectopoo;
+
+import javax.swing.JFrame;
+
 /**
  * @autores Aldo J Márquez, Mateo Pacheco, Luis Suárez, Juan E Muñoz
  */
 public class ProyectoPOO {
 
-    public static void main(String[] args) {
-        
-    }
+    public class Application extends JFrame {
     
+    public Application() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+        setSize(1024, 512);
+        setTitle("Ejercicio 1");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }    
+    
+    public void main(String[] args) {
+        Application ex = new Application();
+        ex.setVisible(true);
+       
+    }
+  }
 }
