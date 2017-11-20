f3b975f0d0cb28f3a30e2e6347c399f8e77bba86
lenovo <lenovo@10.203.161.244>
2017-11-20 16:30:18 -0500

    Vea

--- src/proyectopoo/Menu_Inicio.java
+++ src/proyectopoo/Menu_Inicio.java
@@ -76,7 +76,6 @@ public class Menu_Inicio extends JPanel implements ActionListener {
         super.paintComponent(g);
         Image fondo = loadImage("FONDO MENU.jpg");
         g.drawImage(fondo, 0, 0, 1280, 720, 0, 0, 1752, 1054, this);
-   
     }
 
    
